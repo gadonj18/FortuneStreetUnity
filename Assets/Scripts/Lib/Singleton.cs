ce24731957087d608813d3b376dcd05aff8be649
Al Capwn <gadonj18@live.com>
2015-05-09 22:11:54 -0600

    Yeah fuck all those debug messages

--- Assets/Scripts/Lib/Singleton.cs
+++ Assets/Scripts/Lib/Singleton.cs
@@ -18,9 +18,6 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 		get
 		{
 			if (applicationIsQuitting) {
-				Debug.LogWarning("[Singleton] Instance '"+ typeof(T) +
-				                 "' already destroyed on application quit." +
-				                 " Won't create again - returning null.");
 				return null;
 			}
 			
@@ -32,9 +29,6 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 					
 					if ( FindObjectsOfType(typeof(T)).Length > 1 )
 					{
-						Debug.LogError("[Singleton] Something went really wrong " +
-						               " - there should never be more than 1 singleton!" +
-						               " Reopening the scene might fix it.");
 						return _instance;
 					}
 					
@@ -45,13 +39,6 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 						singleton.name = "(singleton) "+ typeof(T).ToString();
 						
 						DontDestroyOnLoad(singleton);
-						
-						Debug.Log("[Singleton] An instance of " + typeof(T) + 
-						          " is needed in the scene, so '" + singleton +
-						          "' was created with DontDestroyOnLoad.");
-					} else {
-						Debug.Log("[Singleton] Using instance already created: " +
-						          _instance.gameObject.name);
 					}
 				}
 				
