850d737813702f5f824bec3143ff1107b2692eea
Al Capwn <gadonj18@live.com>
2015-05-10 11:50:21 -0600

    Had to rename PassTile to MoveToTile for clarity. Also combined chained FindChild calls into single call.

--- Assets/Scripts/TileActions/PropertyActions.cs
+++ Assets/Scripts/TileActions/PropertyActions.cs
@@ -9,11 +9,11 @@ public class PropertyActions : BaseTileActions {
 		TileScript = Tile.GetComponent<Property>();
 	}
 
-	public override IEnumerator PassTile(bool reversing = false) {
-		UIManager.Instance.UnownedPropertyInfo.transform.Find("District").Find("Text").GetComponent<Text>().text = "District " + TileScript.District;
-		UIManager.Instance.UnownedPropertyInfo.transform.Find("ShopValueBG").Find("ShopValue").GetComponent<Text>().text = "$" + TileScript.ShopValue;
-		UIManager.Instance.UnownedPropertyInfo.transform.Find("ShopPriceBG").Find("ShopPrice").GetComponent<Text>().text = "$" + TileScript.ShopPrice;
-		UIManager.Instance.UnownedPropertyInfo.transform.Find("Name").GetComponent<Text>().text = TileScript.PropertyName;
+	public override IEnumerator MoveToTile(bool reversing = false) {
+		UIManager.Instance.UnownedPropertyInfo.transform.FindChild("District/Text").GetComponent<Text>().text = "District " + TileScript.District;
+		UIManager.Instance.UnownedPropertyInfo.transform.FindChild("ShopValueBG/ShopValue").GetComponent<Text>().text = "$" + TileScript.ShopValue;
+		UIManager.Instance.UnownedPropertyInfo.transform.FindChild("ShopPriceBG/ShopPrice").GetComponent<Text>().text = "$" + TileScript.ShopPrice;
+		UIManager.Instance.UnownedPropertyInfo.transform.FindChild("Name").GetComponent<Text>().text = TileScript.PropertyName;
 		UIManager.Instance.UnownedPropertyInfo.SetActive(true);
 		yield break;
 	}
